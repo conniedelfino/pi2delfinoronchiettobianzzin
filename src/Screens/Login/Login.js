6c415fedfa7900c8ba713dc4093416b79062e11b
conniedelfino <cdelfinochevallier@udesa.edu.ar>
2026-06-11 23:51:30 -0300

    login estilos

--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -19,20 +19,62 @@ function Login(props){
     }
 
     return(
-        <View>
-            <Text>Login</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Login</Text>
+            <TextInput  style={styles.input} 
+                    keyboardType='email-address' 
+                    placeholder='Ingresa aqui tu email' 
+                    onChangeText={text => setEmail(text)} 
+                    value={email}/>
 
-            <Pressable onPress={()=> props.navigation.navigate("Register")}>
-                <Text>Ir a Register</Text>
+            <TextInput  style={styles.input} 
+                    placeholder='Ingresa aqui tu contraseña' 
+                    onChangeText={text => setPassword(text)} 
+                    value={password}
+                    secureTextEntry={true}/>
+
+
+            <Pressable style={styles.button} onPress={()=> onSubmit()}>
+                <Text style={styles.buttonTexto}>Entrar a la App</Text>
             </Pressable>
-            <Pressable onPress={()=> props.navigation.navigate("Post")}>
-                <Text>Ir a Post</Text>
+
+            <Pressable style={styles.button} onPress={()=> props.navigation.navigate("Register")}>
+                <Text style={styles.buttonTextoRegister}>No tengo cuenta</Text>
             </Pressable>
-            <Pressable onPress={()=> props.navigation.navigate("Navegacion")}>
-                <Text>Entrar a la App</Text>
-            </Pressable>
+        
+            
         </View>
-    )
+    );
 }
 
-export default Login
+const styles = StyleSheet.create({
+    container:{
+        padding: 20,
+        flex: 1,
+        justifyContent: "center"
+    },
+    title:{
+        fontSize: 25,
+        margin: 10,
+        textAlign: "center"
+    },
+    input:{
+        borderColor: "black",
+        padding: 15,
+        margin: 5
+    }, 
+    button:{
+        backgroundColor: "green",
+        padding: 15,
+        borderRadius: 7
+    },
+    buttonTexto:{
+        textAlign: "center",
+        color: "red"
+    },
+    error:{
+        color: "red"
+    }
+})
+
+export default Login;
