3571d19aaafffd06d653470f5acef95e287cd97b
mronchiettomeilan <mronchiettomeilan  git config --global user.email mronchiettomeilan@udesa.edu.ar>
2026-06-15 14:46:57 -0300

    styles post y card

--- src/Components/CardPost/CardPost.js
+++ src/Components/CardPost/CardPost.js
@@ -81,7 +81,7 @@ const styles = StyleSheet.create({
         marginBottom: 5,
     },
     textComent:{
-        color: #fff,
+        color: "#fff",
         fontWeight: "bold",
     },
     botonComent:{
--- src/Screens/Post/Post.js
+++ src/Screens/Post/Post.js
@@ -51,36 +51,38 @@ function Post(){
 const styles = StyleSheet.create({
     container:{
         backgroundColor:"#eee",
-        padding:10,
+        padding:20,
         margin:10,
+        borderRadius:10,
     },
     title:{
-        fontSize: 30,
+        fontSize: 28,
         fontWeight:"bold",
-        marginBottom:10
+        marginBottom:15,
+        textAlign: "center"
     },
     textForm:{
         color: "#fff",
+        fontWeight: "bold",
     },
     botonForm:{
-        backgroundColor: "#28a745",
+        backgroundColor: "lightblue",
         paddingHorizontal: 10,
-        paddingVertical: 6,
+        paddingVertical: 10,
         alignItems: "center",
-        borderRadius: 4,
-        borderWidth: 1,
-        borderStyle: "solid",
-        borderColor: "#28a745",
+        borderRadius: 6,
+        marginTop: 10,
     },
     input:{
-        height: 100,
+        height: 120,
         paddingVertical: 15,
         paddingHorizontal: 10,
         borderWidth: 1,
         borderColor: "#ccc",
         borderStyle: "solid",
         borderRadius: 6,
-        marginVertical: 10, 
+        marginVertical: 10,
+        backgroundColor: "#fff"
     }
 })
 
